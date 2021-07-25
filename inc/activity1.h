06c795b740e7fc9847f6c0b23dac8c75c21de1a6
vanshika ujjain <86082546+vanshika1103@users.noreply.github.com>
2021-07-25 16:26:54 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,1 +1,16 @@
 
+#ifndef ACTIVITY1_H_INCLUDED
+#define ACTIVITY1_H_INCLUDED
+/**
+ * @brief function for configuring the registers
+ * @param DDRx for denoting the direction of registers (i.e input or output)
+ * @param PORTx for setting the bit at the respective register (i.e logic HIGH or logic LOW)
+ */
+void pin_config();
+
+/**
+ * @brief function for determining the status of LED actuator depending on the status of both passenger seat and heater button 
+ * @return int 
+ */
+void Activity1();
+#endif // ACTIVITY1_H_INCLUDED
