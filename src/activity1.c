e1b539857a58258c35525bffc4383f80135671d8
vanshika ujjain <86082546+vanshika1103@users.noreply.github.com>
2021-07-25 09:27:29 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,1 +1,42 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "Activity3.h"
+#include "Activity4.h"
+#include "Activity2.h"
 
+
+
+void Activity1()
+
+{
+
+
+    // Direction of port
+    DDRB |= (1 << PB0);
+    DDRD &= ~(1 << PD0);
+    DDRD &= ~(1 << PD4);
+
+    // PULL-UP configuration
+    PORTD |= (1 << PD0);
+    PORTD |= (1 << PD4);
+    while (1)
+    {
+        // condition to check if both switches are on
+        if (!(PIND&(1<<PD0)) && !(PIND&(1<<PD4)))
+        {
+            // set LED
+            PORTB |= (1 << PB0);
+            _delay_ms(200);
+            Activity2();
+            Activity3();
+            Activity4();
+        }
+        else
+        {
+            // set off led
+            PORTB &= ~(1 << PB0);
+            _delay_ms(200);
+        }
+
+    }
+}
