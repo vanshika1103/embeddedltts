830282c3ccb068f5ec8e2f7a24e9e371d22ef044
vanshika ujjain <86082546+vanshika1103@users.noreply.github.com>
2021-07-25 16:28:51 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,1 +1,21 @@
+#ifndef ACTIVITY4_H_INCLUDED
+#define ACTIVITY4_H_INCLUDED
 
+void USARTInit(uint16_t ubrr_value);
+/**
+ * @brief for receiving data
+ *
+ */
+char USARTReadChar();
+/**
+ * @brief For transmitting data
+ *
+ */
+void USARTWriteChar(char data);
+/**
+ * @brief For defining activity4
+ *
+ */
+void Activity4();
+
+#endif // ACTIVITY4_H_INCLUDED
