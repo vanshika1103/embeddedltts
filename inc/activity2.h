7c123a531cc72bc7ffa7db8a0890b16c7b702b9f
vanshika ujjain <86082546+vanshika1103@users.noreply.github.com>
2021-07-25 16:27:36 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,1 +1,22 @@
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+/**
+ * @brief For ARef=Avcc and enabling ADC
+ *
+ */
+void InitADC();
+/**
+ * @brief For Selecting ADC channel,single conversion,and clearing ADIF
+ *
+ */
+uint16_t ReadADC(uint8_t ch);
+/**
+ * @brief Will define activity2
+ *
+ */
+void Activity2();
 
+
+
+
+#endif // ACTIVITY2_H_INCLUDED
